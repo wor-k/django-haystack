806f985ba5c0162503139721ec05ce5ce88bbe64
Worathep Kamsang <worathep@acommerce.asia>
2016-05-04 15:58:11 +0700

    feat(mongo): to supported text_th

--- haystack/backends/solr_backend.py
+++ haystack/backends/solr_backend.py
@@ -464,6 +464,8 @@ class SolrSearchBackend(BaseSearchBackend):
                 field_data['type'] = 'edge_ngram'
             elif field_class.field_type == 'location':
                 field_data['type'] = 'location'
+            elif field_class.field_type in ['text_th']:
+                field_data['type'] = field_class.field_type
 
             if field_class.is_multivalued:
                 field_data['multi_valued'] = 'true'
